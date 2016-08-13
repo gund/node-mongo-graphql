224b7e7a814b694dc294481f0707eb1d40d2221f
Alex Malkevich <malkevich.alex@gmail.com>
2016-08-13 20:42:54 +0400

    Update .gitignore to add custom typings

---
+++ app/typings/index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by alex on 8/13/2016.
+ */
+
+declare module '@risingstack/graffiti' {
+  export default {express:Function};
+}
+
+declare module '@risingstack/graffiti-mongoose' {
+  var getSchema;
+}
